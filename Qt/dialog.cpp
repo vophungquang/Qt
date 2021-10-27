8c25a56126ed1828004a7f800f21768dd59ea7e0
vophungquang <vophungquang123@gmail.com>
2021-10-27 10:44:21 +0700

    use ComboBox

--- Qt/dialog.cpp
+++ Qt/dialog.cpp
@@ -13,47 +13,8 @@ Dialog::~Dialog()
     delete ui;
 }
 
-
-void Dialog::on_pushButton_clicked()
-{
-//    ui->label->setText("Hello");
-//    ui->pushButton->hide();
-}
-
-
-void Dialog::on_lineEdit_editingFinished()
-{
-    ui->label->setText(ui->lineEdit->text());
-}
-
-
 void Dialog::on_pushButton_released()
 {
-    QString text;
-    QString s="You like ";
-    if(ui->cat->isChecked())
-    {
-        if(!text.isEmpty())
-            text+=" and Cat";
-        else
-            text+="Cat";
-    }
-    if(ui->dog->isChecked())
-    {
-        if(!text.isEmpty())
-            text+=" and Dog";
-        else
-            text+="Dog";
-    }
-    if(ui->mouse->isChecked())
-    {
-        if(!text.isEmpty())
-            text+=" and Hamster";
-        else
-            text+="Hamster";
-    }
-
-
-    ui->label->setText(s+text);
+    ui->label->setText("Bạn chọn "+ui->comboBox->currentText());
 }
 
